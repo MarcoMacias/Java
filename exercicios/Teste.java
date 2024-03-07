f1985399f329a1400a810e590c33f247dd10e274
Marco Antonio <69091930+MarcoMacias@users.noreply.github.com>
2024-03-07 14:54:35 -0300

    Add files via upload

---
+++ exercicios/Teste.java
@@ -0,0 +1,15 @@
+
+package exercicios2;
+
+public class Teste {
+
+    public static void main(String[] args) {
+        
+        Calculadora x = new Calculadora();
+        
+        x.soma();
+        
+    }
+
+    
+}
---
+++ exercicios/calculadora.java
@@ -0,0 +1,18 @@
+
+package exercicios2;
+
+import javax.swing.JOptionPane;
+
+
+public class Calculadora {
+    
+    void soma() {
+        int a = 10;
+        int b = 10;
+        
+        int res = a + b;
+        
+        JOptionPane.showMessageDialog(null, "O valor da soma é: " + res);
+    }
+    
+}
