2c39cf1ab7024ca3c8d650b8edc31f53b2227be4
Marco Antonio <69091930+MarcoMacias@users.noreply.github.com>
2024-03-14 15:05:53 -0300

    Add files via upload

---
+++ exercicios/atividade1403/Atividade1403.java
@@ -0,0 +1,15 @@
+
+package atividade1403;
+
+/**
+ *
+ * @author CAMARGO
+ */
+public class Atividade1403 {
+
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
